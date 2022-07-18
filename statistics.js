9f8045cf60ba3451ea8135ed7e6e758160eb21c0
Eman Riziq <emanriziq@gmail.com>
2022-07-19 02:18:27 +0300

    task13

--- statistics.js
+++ statistics.js
@@ -1,33 +1,19 @@
 "use strict";
 let AllFoodArr = RetriveLocal();
-PrintMenu();
+//parsing from JS
+function RetriveLocal() {
+    var getJson = localStorage.getItem('Foods')
+    if (getJson != null)
+        return JSON.parse(getJson);
+}
+
+RenderMenu();
+
 function RenderMenu() {
     let Table = document.getElementById("myresturant");
-    // let Table=document.createElement('table');
-    // Table.classname=("myresturant");
-
-    // let mainel =document.getElementById('mainid');
-    // mainel.appendChild(Table);
-
-    // let Row1=document.createElement('tr');
-    // let Haeding1=document.createElement('th');
-    // Haeding1.textContent="ID";
-    // let Haeding2=document.createElement('th');
-    // Haeding2.textContent="Food Name";
-    // let Haeding3=document.createElement('th');
-    // Haeding3.textContent="Food Type";
-    // let Haeding4=document.createElement('th');
-    // Haeding4.textContent="Price";
-    // Row1.appendChild(Haeding1);
-    // Row1.appendChild(Haeding2);
-    // Row1.appendChild(Haeding3);
-    // Row1.appendChild(Haeding4);
-    // Table.appendChild(Row1);
-
     for (let i = 0; i < AllFoodArr.length; i++) {
-        // Creating and adding data to second row of the table
-        let Row2 = document.createElement('tr');
-        Row2.id="rowid";
+        let Row = document.createElement('tr');
+        Row.id = "rowid";
 
         let Data1 = document.createElement('td');
         Data1.innerHTML = AllFoodArr[i].ID;
@@ -38,21 +24,81 @@ function RenderMenu() {
         let Data4 = document.createElement('td');
         Data4.innerHTML = AllFoodArr[i].Price;
 
-        Row2.appendChild(Data1);
-        Row2.appendChild(Data2);
-        Row2.appendChild(Data3);
-        Row2.appendChild(Data4);
-        Table.appendChild(Row2);
+        Row.appendChild(Data1);
+        Row.appendChild(Data2);
+        Row.appendChild(Data3);
+        Row.appendChild(Data4);
+        Table.appendChild(Row);
     }
 }
 
-function RetriveLocal() {
-    var getJson = localStorage.getItem('Foods')
-    if (getJson != null)
-        return JSON.parse(getJson);
+const NamesArr = [];//for  food names
+const Prices = [];//for food price
+
+for (let i = 0; i < AllFoodArr.length; i++) {
+    NamesArr[i] = AllFoodArr[i].FoodName;
+    Prices[i] = AllFoodArr[i].Price;
 }
-function PrintMenu() {
-    for (let i = 0; i < AllFoodArr.length; i++) {
-        RenderMenu(AllFoodArr[i]);
+DrawBarChart(NamesArr, Prices)
+
+//Draw Bar chart
+function DrawBarChart(Arr1, Arr2) {
+    const data = {
+        labels: Arr1,
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: '#9EB23B',
+            borderColor: '#9EB23B',
+            data: Arr2,
+        }]
+    };
+    const config = {
+        type: 'bar',
+        data: data,
+        options: {}
+    };
+    const myChart = new Chart(
+        document.getElementById('myChart1'),
+        config
+    );
+}
+
+
+const TypesArr = ["Fruit and vegetables", "Starchy", "Dairy", "Protein", "Fat"];
+const TypesCount = [];
+
+for (let i = 0; i < TypesArr.length; i++) {
+    TypesCount[i]=0;
+    for (let x = 0; x < AllFoodArr.length; x++) {
+        if (TypesArr[i] === AllFoodArr[x].Type) {
+            TypesCount[i]++;    
+        }
     }
+}
+DrawBieChart();
+//Draw the bie chart
+function DrawBieChart() {
+    const data = {
+        labels: TypesArr,
+        datasets: [{
+            label: 'Food Type Chart',
+            data: TypesCount,
+            backgroundColor: [
+                '#FCF9C6',
+                ' #9EB23B',
+                '#C7D36F',
+                '#E0DECA',
+                '#C4DFAA'
+            ],
+            hoverOffset: 4
+        }]
+    };
+    const config = {
+        type: 'pie',
+        data: data,
+    };
+    const myChart = new Chart(
+        document.getElementById('myChart2'),
+        config
+    );
 }
